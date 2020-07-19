3dd85970736d023bfbec8f6ab595faf8b042253b
jackpoz <giacomopoz@gmail.com>
2020-07-19 16:04:21 +0200

    Fix update script appending ',' character to table names

--- setup/tools/clisetup/update.func.php
+++ setup/tools/clisetup/update.func.php
@@ -63,8 +63,8 @@ function update()
 
     sleep(1);
 
-    $sql   = trim($sql)   ? array_unique(explode(' ', trim($sql)))   : [];
-    $build = trim($build) ? array_unique(explode(' ', trim($build))) : [];
+    $sql   = trim($sql)   ? array_unique(explode(', ', trim($sql)))   : [];
+    $build = trim($build) ? array_unique(explode(', ', trim($build))) : [];
 
     if ($sql)
         CLI::write('The following table(s) require syncing: '.implode(', ', $sql));
